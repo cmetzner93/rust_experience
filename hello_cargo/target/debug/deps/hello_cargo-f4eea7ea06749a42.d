916021f2c8ada37b949ca5724ea27a8b9aad598b
cmetzner93 <cmetzner@vols.utk.edu>
2021-02-16 20:58:23 +0100

    hello :)

---
+++ hello_cargo/target/debug/deps/hello_cargo-f4eea7ea06749a42.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Metzner/Desktop/rust/local_rust/hello_cargo/target/debug/deps/hello_cargo-f4eea7ea06749a42: src/main.rs
+
+/mnt/c/Users/Metzner/Desktop/rust/local_rust/hello_cargo/target/debug/deps/hello_cargo-f4eea7ea06749a42.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Metzner/Desktop/rust/local_rust/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/Metzner/Desktop/rust/local_rust/hello_cargo/src/main.rs
